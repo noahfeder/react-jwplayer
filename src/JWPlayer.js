a238ee9124f76f5bf128f0da53a48a8f5d5396f7
Noah Feder <noah.feder@gmail.com>
2016-10-25 16:34:20 -0400

    v0.2.0 static app

--- src/JWPlayer.js
+++ src/JWPlayer.js
@@ -3,11 +3,27 @@ import React, { Component } from 'react';
 export default class JWPlayer extends Component {
   render() {
     return (
-      <div>
-        <h1>
-          HELLO WORLD
-        </h1>
+      <div style={ this.props.wrapperStyle }>
+        <iframe src={ this.props.src } { ...this.props.iframeStyle } />
       </div>
     )
+  }
+}
+
+JWPlayer.defaultProps = {
+  wrapperStyle: {
+    position: 'relative',
+    paddingBottom: '56.25%',
+    overflow: 'hidden'
+  },
+  iframeStyle: {
+    width: '100%',
+    height: '100%',
+    frameBorder: '0',
+    scrolling: 'auto',
+    style: {
+      position: 'absolute',
+    },
+    allowFullScreen: true,
   }
 }
