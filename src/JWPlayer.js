78123f7bc69700ed1c76f9d67c89319e3039afae
Noah Feder <noah.feder@gmail.com>
2016-10-25 14:13:23 -0400

    setting up environment

---
+++ src/JWPlayer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class JWPlayer extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          HELLO WORLD
+        </h1>
+      </div>
+    )
+  }
+}
